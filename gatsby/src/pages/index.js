ce8bbfb4dc04336fb368062832155013296e3d32
Chris Carlson <chriscarlson@microsoft.com>
2022-06-10 09:39:40 -0700

    add video component and video to homepage

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Video from '../components/Video';
 
 export default function HomePage() {
   return (
     <>
       <h1>Home Page</h1>
+      <Video
+        videoSrcURL="https://www.youtube.com/embed/vtYNV56MvxU"
+        videoTitle="The Slacks at the Tractor Tavern in Seattle, WA"
+      />
     </>
   );
 }
---
+++ gatsby/src/styles/components/VideoStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const VideoStyles = styled.div`
+  position: relative;
+  padding-bottom: 56.25%; /* 16:9 */
+  height: 0;
+
+  iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default VideoStyles;
