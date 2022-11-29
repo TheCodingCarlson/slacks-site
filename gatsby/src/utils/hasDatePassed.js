1b217dc3d48ce7e62dbf0c572ae86b8140ed6bfd
Chris Carlson <chriscarlson@microsoft.com>
2022-11-29 15:21:56 -0800

    revert package-lock updates, format code

--- gatsby/src/utils/hasDatePassed.js
+++ gatsby/src/utils/hasDatePassed.js
@@ -4,7 +4,11 @@ const hasDatePassed = (date) => {
   console.log('********************');
   console.log('today:', today);
   console.log('gig date:', date);
-  console.log(date.setHours(0, 0, 0, 0) <= today.setHours(0, 0, 0, 0) ? 'The gig date is in the past' : 'The gig date is in the future' );
+  console.log(
+    date.setHours(0, 0, 0, 0) <= today.setHours(0, 0, 0, 0)
+      ? 'The gig date is in the past'
+      : 'The gig date is in the future'
+  );
 
   return date.setHours(0, 0, 0, 0) <= today.setHours(0, 0, 0, 0);
 };
